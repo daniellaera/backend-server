e6c23ac507adb96e3c904756101257d367964785
nomades 037 <nomades037@nomadess-iMac.local>
2018-06-26 11:55:57 +0200

    course commit

--- routes/api/course.js
+++ routes/api/course.js
@@ -18,42 +18,42 @@ router.post('/add',(req, res) => {
 });
 
 router.route('/').get((req, res) => {
-    Course.find((err, courses) => {
-      if(err){
-        console.log(err);
-      }
-      else {
-        res.json(courses);
-      }
-    });
+  Course.find((err, courses) => {
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.json(courses);
+    }
   });
+});
 
-  // update course 
-  router.post('/update/:id', (req, res) => {
-    Course.findById(req.params.id, (err, course) => {
-      if (!course)
-        return next(new Error('Could not load Document'));
-      else {
-        course.course_name = req.body.course_name;
-        course.course_price = req.body.course_price;
+// update course 
+router.post('/update/:id', (req, res) => {
+  Course.findById(req.params.id, (err, course) => {
+    if (!course)
+      return next(new Error('Could not load Document'));
+    else {
+      course.course_name = req.body.course_name;
+      course.course_price = req.body.course_price;
   
-        course.save().then(course => {
-            res.json('Successfully Updated');
-        })
-        .catch(err => {
-              res.status(400).send("unable to update the database");
-        });
-      }
-    });
+      course.save().then(course => {
+        res.json('Successfully Updated');
+      })
+      .catch(err => {
+        res.status(400).send("unable to update the database");
+      });
+    }
   });
+});
 
 
 //delete post  
 router.delete('/:id', (req, res) => { 
   Course.findById(req.params.id)
   .then(course => {
-      // Delete
-      course.remove().then(() => res.json({ msg: 'successfully deleted'}));
+    // Delete
+    course.remove().then(() => res.json({ msg: 'successfully deleted'}));
   })
   .catch(err => res.status(404).json({ coursenotfound: 'No post found'}));
 });
